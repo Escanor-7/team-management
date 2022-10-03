2a743971ead26457c803596d89de918572419df2
Escanor-7 <carlos.escanor01@gmail.com>
2022-10-03 19:37:31 -0300

    Application font configuration

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import { Groups } from '@screens/Groups';
+import { ActivityIndicator, StyleSheet } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Groups } from '@screens/Groups';
+
 import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme} >
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator size={50} style={secundaryStyles.loading} />}
     </ThemeProvider>
   );
 }
+
+const secundaryStyles = StyleSheet.create({
+  loading: {
+    flex: 1,
+    backgroundColor: '#202024',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,7 +3,6 @@ import * as S from "./styles";
 export function Groups() {
   return (
     <S.Container >
-      <S.Title >Groups</S.Title>
     </S.Container>
   )
 }
