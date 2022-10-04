b480094fec9fe64b33d542c3b79f015d79a8e733
Escanor-7 <carlos.escanor01@gmail.com>
2022-10-04 17:21:03 -0300

    Toggle button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Delta', 'Alpha', 'Omega']);
@@ -23,6 +24,9 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message='Que tal cadastrar a primeira turma?' />
         )}
+      />
+      <Button
+        title='Criar nova turma'
       />
     </S.Container>
   )
