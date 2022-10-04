c56db8d83f39df5573076146a68933dc810e9c2b
Escanor-7 <carlos.escanor01@gmail.com>
2022-10-04 16:32:20 -0300

    Creating the GroupCard Component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,14 @@
 import * as S from "./styles";
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <S.Container >
       <Header showBackButton />
+      <Highlight title='Turmas' subtitle='Jogue com sua turma' />
+      <GroupCard title='Galera do Ignite' />
     </S.Container>
   )
 }
