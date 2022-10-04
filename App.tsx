23b1143c254405301152c69240fddc3816b5ac02
Escanor-7 <carlos.escanor01@gmail.com>
2022-10-04 15:14:00 -0300

    Adding icon library

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator, StatusBar, StyleSheet } from 'react-native';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,10 @@
 import * as S from "./styles";
+import { Header } from "@components/Header";
 
 export function Groups() {
   return (
     <S.Container >
+      <Header showBackButton />
     </S.Container>
   )
 }
