3955e9b838560e1f390413bf24101ac0d78cfb6b
Escanor-7 <carlos.escanor01@gmail.com>
2022-10-04 17:00:57 -0300

    List empty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import * as S from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Delta', 'Bravos', 'Alpha']);
+  const [groups, setGroups] = useState<string[]>(['Delta', 'Alpha', 'Omega']);
 
   return (
     <S.Container >
@@ -17,6 +18,10 @@ export function Groups() {
         keyExtractor={(item) => item}
         renderItem={({ item }) => (
           <GroupCard title={item} />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Que tal cadastrar a primeira turma?' />
         )}
       />
     </S.Container>
