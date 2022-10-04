5767fe093e92f5b78a4283da6a1de68431cf8863
Escanor-7 <carlos.escanor01@gmail.com>
2022-10-04 16:51:14 -0300

    Implementing the list of groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import * as S from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Delta', 'Bravos', 'Alpha']);
+
   return (
     <S.Container >
       <Header showBackButton />
       <Highlight title='Turmas' subtitle='Jogue com sua turma' />
-      <GroupCard title='Galera do Ignite' />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+      />
     </S.Container>
   )
 }
